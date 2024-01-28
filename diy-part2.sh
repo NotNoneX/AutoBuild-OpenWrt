616dc699ec0782f1b869ac590fc808bc48736d84
Einstar <63887027+NotNoneX@users.noreply.github.com>
2024-01-28 21:15:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,23 +13,27 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-# ----- 自定义部分 -----
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+# --------------- 自定义部分 ---------------
+# 修改默认主题
+$WRT_THEME=argon
+sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+## 配置文件修改
+echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 
-#修改默认主机名
+# 修改默认主机名
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" $CFG_FILE
-#修改默认时区
+# 修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
-#修改默认WIFI名
+# 修改默认WIFI名
 sed -i "s/ssid=.*/ssid=冰糖/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # ddns-go软件
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 
-# ----- end -----
+# --------------- end ---------------
 
