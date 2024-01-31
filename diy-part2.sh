220dfed4c9a24e99adfaf703d87d740985a4d8ee
Einstar <63887027+NotNoneX@users.noreply.github.com>
2024-01-31 22:35:51 +0800

    Update diy-part2.sh
    默认主题修改测试

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,12 +14,16 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # --------------- 自定义部分 ---------------
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+# find ./ -name luci-theme-argon | xargs rm -rf;
+
 # 修改默认主题
-# WRT_THEME=argon
-# sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-## 配置文件修改 或手动添加
-#echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+# 针对 LEDE 项目拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 # 修改默认主机名
@@ -28,8 +32,8 @@ sed -i "s/hostname='.*'/hostname='OpenWrt'/g" $CFG_FILE
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
-# 修改默认WIFI名
-sed -i "s/ssid=.*/ssid=冰糖/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认WIFI名 Einstar
+sed -i "s/ssid=.*/ssid=Einstar/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # ddns-go软件
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
