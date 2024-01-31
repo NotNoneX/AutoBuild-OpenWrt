720da76cda8bd4557c10d775d37fe703735089fc
Einstar <63887027+NotNoneX@users.noreply.github.com>
2024-02-01 07:07:00 +0800

    更新 diy-part2.sh
    默认主题设置

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,5 +28,17 @@ sed -i "s/ssid=.*/ssid=Einstar/g" ./package/kernel/mac80211/files/lib/wifi/mac80
 # ddns-go软件
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+# find ./ -name luci-theme-argon | xargs rm -rf;
+
+# 修改默认主题
+# 针对 LEDE 项目拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+
 # --------------- end ---------------
 
