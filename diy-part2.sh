fdff92ea54e0c480171cbfe7b060838dbe49bbc0
Einstar <63887027+NotNoneX@users.noreply.github.com>
2024-02-01 12:58:55 +0800

    更新 diy-part2.sh
    Argon主题设置

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,8 +37,7 @@ rm -rf package/lean/luci-theme-argon
 # 针对 LEDE 项目拉取 argon 原作者的源码
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # 替换默认主题为 luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # --------------- end ---------------
 
