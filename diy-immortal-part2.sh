8d3526535c5fc3ec5fbb515449033d6c0fda1f67
Einstar <63887027+NotNoneX@users.noreply.github.com>
2024-02-25 14:41:16 +0800

    Update diy-immortal-part2.sh
    pushbot无法启动 取消编译

--- diy-immortal-part2.sh
+++ diy-immortal-part2.sh
@@ -14,6 +14,13 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # --------------- 自定义部分 ---------------
+# 配置文件修改 主题及主题配置
+# echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
+# echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+
+# 汉化 immortal主分支
+# echo "CONFIG_PACKAGE_luci=y" >> ./.config
+# echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 # 修改默认主机名
@@ -22,24 +29,19 @@ sed -i "s/hostname='.*'/hostname='OpenWrt'/g" $CFG_FILE
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
+
 # 修改默认WIFI名 Einstar
 sed -i "s/ssid=.*/ssid=Einstar/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认WIFI名 适用于immortal主分支master
+# sed -i "s/ssid=.*/ssid=Einstar/g" ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 
-# ddns-go软件
+# ddns-go软件 immortal自带 故注释
 # git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 
-# pushbot插件
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+# pushbot插件 无法启动 先注释
+# git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 
-# 删除自定义源默认的 argon 主题
-# rm -rf package/lean/luci-theme-argon
-# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
-# find ./ -name luci-theme-argon | xargs rm -rf;
-
-# 修改默认主题
-# 针对 LEDE 项目拉取 argon 原作者的源码
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-# 替换默认主题为 luci-theme-argon
+# 修改默认主题 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # --------------- end ---------------
