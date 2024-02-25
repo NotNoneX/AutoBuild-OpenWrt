e238c2842b9e9b1da9da0f682f120942e05af0cf
Einstar <63887027+NotNoneX@users.noreply.github.com>
2024-02-25 12:21:35 +0800

    Update diy-immortal-part2.sh

--- diy-immortal-part2.sh
+++ diy-immortal-part2.sh
@@ -1,1 +1,43 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+# --------------- 自定义部分 ---------------
+
+CFG_FILE="./package/base-files/files/bin/config_generate"
+# 修改默认主机名
+sed -i "s/hostname='.*'/hostname='OpenWrt'/g" $CFG_FILE
+# 修改默认时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
+sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
+
+# 修改默认WIFI名 Einstar
+sed -i "s/ssid=.*/ssid=Einstar/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# ddns-go软件
+# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
+# 删除自定义源默认的 argon 主题
+# rm -rf package/lean/luci-theme-argon
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+# find ./ -name luci-theme-argon | xargs rm -rf;
+
+# 修改默认主题
+# 针对 LEDE 项目拉取 argon 原作者的源码
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
+# --------------- end ---------------
 
