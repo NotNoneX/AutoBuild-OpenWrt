6aa99bfb087d6fb76f8d2194273020038ccd66e1
Einstar <63887027+NotNoneX@users.noreply.github.com>
2024-07-29 20:12:14 +0800

    Update diy-immortal-part1.sh

--- diy-immortal-part1.sh
+++ diy-immortal-part1.sh
@@ -17,5 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # Passwall 2
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
